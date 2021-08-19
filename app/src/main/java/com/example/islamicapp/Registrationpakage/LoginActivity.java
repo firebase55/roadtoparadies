c26e0fbeae555c24a8da6eb10a0cf3160181925e
DESKTOP-CTVMTKB <firebase732@gmail.com>
2021-08-19 12:45:35 -0700

    firebase backend

--- app/src/main/java/com/example/islamicapp/Registrationpakage/LoginActivity.java
+++ app/src/main/java/com/example/islamicapp/Registrationpakage/LoginActivity.java
@@ -1,34 +1,122 @@
 package com.example.islamicapp.Registrationpakage;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.islamicapp.BoyGirlPanel.biy_girl_panel.boygirlPanelActivity;
 import com.example.islamicapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class LoginActivity extends AppCompatActivity {
-private Button Main_panel_boy_girl;
+    private Button Main_panel_boy_girl;
+    private EditText Email, Pass;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Main_panel_boy_girl = (Button)findViewById(R.id.boy_girls_panel);
+        Main_panel_boy_girl = (Button) findViewById(R.id.boy_girls_panel);
+        Email = (EditText) findViewById(R.id.email_id);
+        Pass = (EditText) findViewById(R.id.pass_id);
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseApp.initializeApp(this);
         onclick_function();
 
     }
-    public void onclick_function()
-    {
+
+    public void onclick_function() {
         Main_panel_boy_girl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), boygirlPanelActivity.class);
-                startActivity(intent);
+                String Emai_field = Email.getText().toString().trim();
+                String Pass_field = Pass.getText().toString().trim();
+                if (Emai_field.isEmpty() || Pass_field.isEmpty()) {
+                    new SweetAlertDialog(LoginActivity.this, SweetAlertDialog.ERROR_TYPE)
+                            .setTitleText("Please fill Your data")
+                            .setConfirmText("Ok")
+                            .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sDialog) {
+
+                                    sDialog.dismissWithAnimation();
+
+
+                                }
+                            }).show();
+
+                } else {
+                    mAuth.signInWithEmailAndPassword(Emai_field, Pass_field).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                Intent intent = new Intent(getApplicationContext(), boygirlPanelActivity.class);
+                                startActivity(intent);
+                            }
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+                }
             }
         });
+    }
+
+    public void forget_pass(View view) {
+        final EditText resetMail = new EditText(view.getContext());
+        final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(view.getContext());
+        passwordResetDialog.setTitle("Reset Password ?");
+        passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+        passwordResetDialog.setView(resetMail);
+        passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // extract the email and send reset link
+                String mail = resetMail.getText().toString();
+                mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(LoginActivity.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+        });
+        passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // close the dialog
+            }
+        });
+
+        passwordResetDialog.create().show();
 
     }
 }
